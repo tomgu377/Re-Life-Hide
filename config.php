d728477efe08f92654793920e809defe7f43711a
tomgu377 <tomgu377@gmail.com>
2024-09-06 14:56:25 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$config = [
+    'PROXY_USERPW'      => true,
+    'PROXY_LIST'        => 'server.txt',
+    'PROXY_AUTH'        => '9Er4wtsQN1:ypZ795VIFc',
+    'PROXY_TYPE'        => 'SOCKS5', // select: HTTP / HTTP_1_0 / HTTPS / SOCKS4 / SOCKS4A / SOCKS5_HOSTNAME / SOCKS5
+    'APP_VERSION'       => '39.0.0.1',
+    'DELAY'             => 0,
+    'orderid'           => 'ccd7e1b4-6e4e-4c57-b19d-8393661003e3',
+];
