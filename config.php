fe46cd01041ef1321bc7c6cefa8f52333060e498
tomgu377 <tomgu377@gmail.com>
2024-09-10 10:42:03 +0700

    Add files via upload

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 $config = [
-    'PROXY_USERPW'      => true,
-    'PROXY_LIST'        => 'server.txt',
-    'PROXY_AUTH'        => '9Er4wtsQN1:ypZ795VIFc',
-    'PROXY_TYPE'        => 'SOCKS5', // select: HTTP / HTTP_1_0 / HTTPS / SOCKS4 / SOCKS4A / SOCKS5_HOSTNAME / SOCKS5
-    'APP_VERSION'       => '39.0.0.1',
-    'DELAY'             => 0,
-    'orderid'           => 'ccd7e1b4-6e4e-4c57-b19d-8393661003e3',
+    'APIKEY'            => 'YOU APIKEY',            // YOU APIKEY
+    'PROXY_USERPW'      => true,                    // PROXY WITH AUTH USERNAME:PASSWORD
+    'PROXY_LIST'        => 'proxy.txt',             // PROXY LIST
+    'PROXY_AUTH'        => 'USERNAME:PASSWORD',     // PROXY AUTH
+    'PROXY_TYPE'        => 'SOCKS5',                // select: HTTP / HTTP_1_0 / HTTPS / SOCKS4 / SOCKS4A / SOCKS5_HOSTNAME / SOCKS5
+    'APP_VERSION'       => '39.0.0.1',              // APP VERSION
+    'orderid'           => 'YOU ORDERID',           // ORDER ID PESANAN
 ];
